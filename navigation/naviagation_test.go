fcb47db75b7104efe07dec55c3891d2018810715
mbparis <briceparis@gmail.com>
2021-12-15 19:28:05 -0600

    Navigate

--- navigation/naviagation_test.go
+++ navigation/naviagation_test.go
@@ -15,3 +15,24 @@ func TestPosition(t *testing.T) {
 	}
 
 }
+
+func TestNavigate(t *testing.T) {
+	expected := 150
+
+	position := Position{x: 0, y: 0}
+
+	position.forward(5)
+	position.down(5)
+	position.forward(8)
+	position.up(3)
+	position.down(8)
+	position.forward(2)
+
+	actual := position.check()
+
+	if expected != actual {
+		t.Errorf("Position.check() = %v; want %v", actual, expected)
+
+	}
+
+}
--- navigation/navigation.go
+++ navigation/navigation.go
@@ -8,3 +8,15 @@ type Position struct {
 func (p Position) check() int {
 	return p.x * p.y
 }
+
+func (p *Position) forward(units int) {
+	p.x = p.x + units
+}
+
+func (p *Position) down(units int) {
+	p.y = p.y + units
+}
+
+func (p *Position) up(units int) {
+	p.y = p.y - units
+}
